7e1324bcf63bd2d0b808f8d8cfd7d1b41dcbae5f
IsaacGonzalez1 <Isaac.Gonzalez1@alu.uclm.es>
2022-11-29 23:42:39 +0100

    login provisional, quedan muchas cosas por meter

--- Rutas-Senderismo-Hito2/Rutas-Senderismo-Hito2/MainWindow.xaml.cs
+++ Rutas-Senderismo-Hito2/Rutas-Senderismo-Hito2/MainWindow.xaml.cs
@@ -20,9 +20,63 @@ namespace Rutas_Senderismo_Hito2
     /// </summary>
     public partial class MainWindow : Window
     {
+        private Usuarios admin = new Usuarios("Isaac", "Gonzalez", "admin1", "admin1");
+        private Usuarios admin2 = new Usuarios("Natalia", "Jimenez", "admin2", "admin2");
+        private BitmapImage imagCheck = new BitmapImage(new Uri("/Imagenes/check.png", UriKind.Relative));
+        private BitmapImage imagCross = new BitmapImage(new Uri("/Imagenes/cross.png", UriKind.Relative));
+        //arreglar source de las imágenes
         public MainWindow()
         {
             InitializeComponent();
+        }
+        private void Window_Closing(object sender, System.ComponentModel.CancelEventArgs e)
+        {
+            Application.Current.Shutdown();
+        }
+
+        private void txtUsuario_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.Key == Key.Return)
+            {
+
+                if (!String.IsNullOrEmpty(txtUsuario.Text)
+                && (ComprobarEntrada(txtUsuario.Text, admin.Usuario,
+                txtUsuario, imgCheckUsuario) || ComprobarEntrada(txtUsuario.Text, admin2.Usuario,
+                txtUsuario, imgCheckUsuario) ))
+                {
+                    // habilitar entrada de contraseña y pasarle el foco
+                    ContraseñaBox.IsEnabled = true;
+                    ContraseñaBox.Focus();
+                    // deshabilitar entrada de login
+                    txtUsuario.IsEnabled = false;
+                }
+            }
+
+        }
+
+
+        private Boolean ComprobarEntrada(string valorIntroducido, string valorValido, Control componenteEntrada, Image imagenFeedBack)
+        {
+            Boolean valido = false;
+            componenteEntrada.BorderThickness = new Thickness(2);
+            if (valorIntroducido.Equals(valorValido))
+            {
+                // borde y background en verde
+                componenteEntrada.BorderBrush = Brushes.Green;
+                componenteEntrada.Background = Brushes.LightGreen;
+                // imagen al lado de la entrada de usuario --> check
+                imagenFeedBack.Source = imagCheck;
+                valido = true;
+            }
+            else
+            {
+                // marcamos borde en rojo
+                componenteEntrada.BorderBrush = Brushes.Red;
+                // imagen al lado de la entrada de usuario --> cross
+                imagenFeedBack.Source = imagCross;
+                valido = false;
+            }
+            return valido;
         }
     }
 }
---
+++ Rutas-Senderismo-Hito2/Rutas-Senderismo-Hito2/Usuarios.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Rutas_Senderismo_Hito2
+{
+    class Usuarios
+    {
+        public string Nombre { set; get; }
+        public string Apellido { set; get; }
+        public string Usuario { set; get; }
+        public string Contrasena { set; get; }
+        public Usuarios(string nombre, string apellido, string usuario, string contrasena)
+        {
+            Nombre = nombre;
+            Apellido = apellido;
+            Usuario = usuario;
+            Contrasena = contrasena;
+        }
+
+
+
+
+
+    }
+}
